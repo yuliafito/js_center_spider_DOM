f8c76147685d5e75a90f8159b2c86c3c1c0f5901
Yuliia Fito <jylia.fito2004@gmail.com>
2024-07-11 09:18:50 +0300

    make build

---
+++ index.008ea289.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=(e.clientHeight-t.clientHeight)/2,l=(e.clientWidth-t.clientWidth)/2;t.style.top="".concat(c,"px"),t.style.left="".concat(l,"px");
+//# sourceMappingURL=index.008ea289.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const topPosition = (wall.clientHeight - spider.clientHeight) / 2;
+const leftPosition = (wall.clientWidth - spider.clientWidth) / 2;
+spider.style.top = `${topPosition}px`;
+spider.style.left = `${leftPosition}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
